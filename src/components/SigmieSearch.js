f8a603a6e353fc41f1d07655a107f05ac9f2e405
Nico Orfanos <nico@sigmie.com>
2023-02-07 14:50:54 +0200

    fix: remove d suffix

--- src/components/SigmieSearch.js
+++ src/components/SigmieSearch.js
@@ -2,48 +2,45 @@ import { useState, useEffect } from 'react'
 
 function SigmieSearch(props) {
 
-  const [state, setState] = useState({ hits: [], total: 0, loading: false })
-
-  let search = function () {
-
-    const body = {
-      query: props.query,
-      per_page: props.perPage,
-      filter: props.filter
-    }
-
-    fetch(`https://${props.applicationId}.sigmie.app/v1/search/${props.search}d`,
-      {
-        method: 'POST',
-        mode: 'cors',
-        cache: 'no-cache',
-        headers: {
-          'Content-Type': 'application/json',
-          'X-Sigmie-API-Key': props.apiKey,
-          'X-Sigmie-Application': props.applicationId,
-        },
-        redirect: 'follow',
-        body: JSON.stringify(body)
-      }).then((response) => {
-        return response.json();
-      })
-      .then((response) => {
-
-        if (response.error) {
-          throw new Error(response.message, { cause: response });
-        }
-
-        setState({
-          hits: response.hits,
-          total: response.total,
-          loading: false,
-        });
-      });
-  };
+  const [state, setState] = useState({ hits: {}, total: 0, loading: false })
 
   useEffect(() => {
+
     const getData = setTimeout(() => {
-      search();
+      const body = {
+        query: props.query,
+        per_page: props.perPage,
+        filter: props.filter
+      }
+
+      fetch(`https://${props.applicationId}.sigmie.app/v1/search/${props.search}`,
+        {
+          method: 'POST',
+          mode: 'cors',
+          cache: 'no-cache',
+          headers: {
+            'Content-Type': 'application/json',
+            'X-Sigmie-API-Key': props.apiKey,
+            'X-Sigmie-Application': props.applicationId,
+          },
+          redirect: 'follow',
+          body: JSON.stringify(body)
+        }).then((response) => {
+          return response.json();
+        })
+        .then((response) => {
+          if (response.error) {
+            throw new Error(response.message, { cause: response });
+          }
+
+          setState({
+            hits: response.hits,
+            total: response.total,
+            loading: false,
+          });
+        });
+
+
     }, props.debounceMs)
 
     return () => {
@@ -57,7 +54,10 @@ function SigmieSearch(props) {
 }
 
 SigmieSearch.defaultProps = {
+  applicationId: '',
+  apiKey: '',
   query: '',
+  search: '',
   filter: '',
   debounceMs: 150,
   perPage: 10,
